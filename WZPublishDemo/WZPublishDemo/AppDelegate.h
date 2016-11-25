3220133b8f49ee44e770edb8f3d25cb1142e68c0
songbiwen <1099790014@qq.com>
2016-11-25 16:13:13 +0800

    + 初始化

---
+++ WZPublishDemo/WZPublishDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WZPublishDemo
+//
+//  Created by songbiwen on 2016/11/25.
+//  Copyright © 2016年 songbiwen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WZPublishDemo/WZPublishDemo/Classes/Controller/WZPublishViewController.m
@@ -0,0 +1,38 @@
+//
+//  WZPublishViewController.m
+//  WZPublishDemo
+//
+//  Created by songbiwen on 2016/11/25.
+//  Copyright © 2016年 songbiwen. All rights reserved.
+//
+
+#import "WZPublishViewController.h"
+
+@interface WZPublishViewController ()
+
+@end
+
+@implementation WZPublishViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.title = @"发布";
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
